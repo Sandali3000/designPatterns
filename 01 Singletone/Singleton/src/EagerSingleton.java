642c9ebdf2463f8d3bfcdac1ad349119edb38dda
W.S.A. Wickramasinghe <127072941+Sandali3000@users.noreply.github.com>
2025-03-11 07:52:44 -0700

    Delete 01 Singletone directory

--- 01 Singletone/Singleton/src/EagerSingleton.java
+++
@@ -1,9 +0,0 @@
-public class EagerSingleton {
-    private static final EagerSingleton instance = new EagerSingleton();
-    private EagerSingleton() {
-        System.out.println("I am EagerSingleton");
-    }
-    public static EagerSingleton getInstance() {
-        return instance;
-    }
-}
